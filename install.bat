35ee5725421cedf69930f372fe6a759495e028ae
The Conceptionist <theconceptionist+github@proton.me>
2023-03-04 21:58:31 -0700

    Add batch files

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+git clone https://github.com/frozenpandaman/s3s.git
+cd s3s
+python -m pip install -r requirements.txt
+echo Done installing! You can now close this window.
+exit
---
+++ update and run.bat
@@ -0,0 +1,4 @@
+@echo off
+title S3S
+git pull
+python s3s.py -M -r
